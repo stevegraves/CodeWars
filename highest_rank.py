65302590e0b211b69c517c08cf4d4bf5025769ad
Steve Graves <steve.graves86@gmail.com>
2020-02-06 22:58:17 -0800

    highest rank

---
+++ highest_rank.py
@@ -0,0 +1,12 @@
+#Highest ranked number in array
+from collections import Counter
+
+
+def highest_rank(arr):
+    if arr:
+        c = Counter(arr)
+        m = max(c.values())
+        return max(k for k, v in c.items() if v == m)
+
+
+
