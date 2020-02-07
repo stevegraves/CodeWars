85b911f93d2100d8b2d4d7d36a06780b7cd916df
Steve Graves <steve.graves86@gmail.com>
2020-02-06 22:24:21 -0800

    count.py

---
+++ count.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+
+# return dictionary of count of characters
+def count(string):
+    # print(Counter(string))
+    return Counter(string)
+
+# count('this is a test')
