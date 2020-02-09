fed2ae3c98244528cb05ceac26130cc020c58957
Steve Graves <steve.graves86@gmail.com>
2020-02-09 14:02:44 -0800

    prime_factors.py

---
+++ prime_gap.py
@@ -0,0 +1,33 @@
+import math
+
+#Boolean primality check with 6k +/- 1 pattern
+def is_prime(n):
+    if n <= 6:
+        return n == 2 or n == 3 or n == 5
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    limit = int(math.sqrt(n))
+    t = 5
+    while t <= limit:
+        if n % t == 0 or n % (t + 2) == 0:
+            return False
+        t += 6
+    return True
+
+
+#find first prime pair of given gap within range
+def gap(g, m, n):
+    previous = None
+    for i in range(m, n + 1):
+        if is_prime(i):
+            if previous:
+                if i - previous == g:
+                    return [previous, i]
+                else:
+                    previous = i
+            else:
+                previous = i
+    return None
+
+
+
