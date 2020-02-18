519e091f9aed5f2b37406b3754408d8d202bb5fa
Steve Graves <steve.graves86@gmail.com>
2020-02-17 21:19:18 -0800

    Fix error in mix2

--- mix.py
+++ mix.py
@@ -56,13 +56,14 @@ def mix2(s1, s2):
     c1 = Counter(filter(str.islower, s1))
     c2 = Counter(filter(str.islower, s2))
     res = []
-    for c in set(c1.keys() + c2.keys()):
+    for c in set(c1 + c2):
         n1, n2 = c1.get(c, 0), c2.get(c, 0)
         if n1 > 1 or n2 > 1:
             res.append(('1', c, n1) if n1 > n2 else
                        ('2', c, n2) if n2 > n1 else ('=', c, n1))
     res = ['{}:{}'.format(i, c * n) for i, c, n in res]
-    return '/'.join(sorted(res, key=lambda s: (-len(s), s)))
+    print(res)
+    print('/'.join(sorted(res, key=lambda s: (-len(s), s))))
 
 # mix("Are they here", "yes, they are here")
 # mix("looping is fun but dangerous", "less dangerous than coding")
