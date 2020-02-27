16c931b121009a49fa2a940e16bbdaf5363fca33
Steve Graves <steve.graves86@gmail.com>
2020-02-26 17:43:40 -0800

    Initial solution for b_f_translator.py

--- b_f_translator.py
+++ b_f_translator.py
@@ -23,13 +23,59 @@ def brainfuck_to_c(source_code):
             output += c
     if '[' in output and ']' not in output or ']' in output and '[' not in output:
         return 'Error!'
+    if output == '':
+        return ''
 
-    print(output)
+    #print(output)
     count = 1
     for i in range(1, len(output)):
         if output[i - 1] == output[i]:
             count += 1
+            if output[i - 1] == '.':
+                output2 += 'putchar(*p);\n'
+            if output[i - 1] == ',':
+                output2 += '*p = getchar();\n'
+            if output[i - 1] == '[':
+                output2 += 'if (*p) do {\n  '
+            if output[i - 1] == ']':
+                output2 += '} while (*p);\n'
         else:
+            if output[i - 1] == '+':
+                output2 += '*p += {};\n'.format(count)
+            if output[i - 1] == '-':
+                output2 += '*p -= {};\n'.format(count)
+            if output[i - 1] == '<':
+                output2 += 'p -= {};\n'.format(count)
+            if output[i - 1] == '>':
+                output2 += 'p += {};\n'.format(count)
+            if output[i - 1] == '.':
+                output2 += 'putchar(*p);\n'
+            if output[i - 1] == ',':
+                output2 += '*p = getchar();\n'
+            if output[i - 1] == '[':
+                output2 += 'if (*p) do {\n  '
+            if output[i - 1] == ']':
+                output2 += '} while (*p);\n'
+            count = 1
+    if output[-1] == '+':
+        output2 += '*p += {};\n'.format(count)
+    if output[-1] == '-':
+        output2 += '*p -= {};\n'.format(count)
+    if output[-1] == '<':
+        output2 += 'p -= {};\n'.format(count)
+    if output[-1] == '>':
+        output2 += 'p += {};\n'.format(count)
+    if output[-1] == '.':
+        output2 += 'putchar(*p);\n'
+    if output[-1] == ',':
+        output2 += '*p = getchar();\n'
+    if output[-1] == '[':
+        output2 += 'if (*p) do {\n  '
+    if output[-1] == ']':
+        output2 += '} while (*p);\n'
+
+    print(output2)
+    return output2
             
 
 
@@ -43,7 +89,7 @@ s_c1 = "++++"
 s_c2 = "----"
 
 brainfuck_to_c(test1)
-brainfuck_to_c(test3)
+#brainfuck_to_c(test3)
 brainfuck_to_c(test2)
-brainfuck_to_c(test4)
+brainfuck_to_c(s_c1)
 
