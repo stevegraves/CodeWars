1c1ac20de2b455066500c9d778d5b11eb9dbfd92
Steve Graves <steve.graves86@gmail.com>
2020-02-27 21:04:41 -0800

    Add initial solution

--- Mongo.py
+++ Mongo.py
@@ -6,10 +6,30 @@ class Mongo(object):
     @classmethod
     def is_valid(cls, s):
         """returns True if s is a valid MongoID; otherwise False"""
-        pass
+        if s and isinstance(s, str) and len(s) == 24:
+            for c in s:
+                if c.isupper():
+                    return False
+            try:
+                int(s, 16)
+                return True
+            except ValueError:
+                return False
+        return False
 
     @classmethod
     def get_timestamp(cls, s):
         """if s is a MongoID, returns a datetime object for the timestamp; otherwise False"""
-        pass
+        if Mongo.is_valid(s):
+            return datetime.fromtimestamp(int(s[:8], 16))
+        return False
 
+Mongo.is_valid('507f1f77bcf86cd799439011')  # True
+Mongo.get_timestamp('507f1f77bcf86cd799439011')  # Wed Oct 17 2012 21:13:27 GMT-0700 (Pacific Daylight Time)
+
+
+x = '507f1f77bcf86cd799439011'[:8]
+y = int(x, 16)
+print(datetime.utcfromtimestamp(y))
+
+print(datetime(2012, 10, 17, 21, 13, 27))
